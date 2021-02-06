492d1552e4cb278eb009178be855cca84e2ddf4d
siebe <siebe.vanzwaal@student.hu.nl>
2021-02-07 00:43:19 +0100

    c

--- mastered mastermind.py
+++ mastered mastermind.py
@@ -11,28 +11,43 @@ def generate():
 def guess(code, amount_guess=0):
     '''lets the player guess and returns amount of white and black pins'''
     playerguess = input('Guess a combination of uppercase letters between A to and with F\n')
+    playerguesslist = list(playerguess)
     pins = {'black':0,'white':0}
-    code1=[]
-    j=0
-    for i in code:
-        code1.append(i)
-    print(code1)
-    if list(playerguess) == code:
+    #print(code)
+    dct = {1:True,2:True,3:True,4:True}
+    if playerguesslist== code:
         return 'U heeft gewonnen'
-    for i in range(len(playerguess)):
-        if playerguess[i] == code[i]:
+    i=0
+    while i < len(playerguesslist):
+        print(i)
+        if playerguesslist[i] == code[i]:
             pins['black'] +=1
-            code1.remove(code1[i])
-    for i in range(len(playerguess)):
-        while code1 !=[] and j <len(code1):#for j in range(len(code1)):
-            #print(code1)
-            if playerguess[i] == code1[j]:
-                pins['white'] +=1
-                code1.remove(code1[j])
+            dct[i] = False
+
+        i+=1
+    i = 0
+    while i < len(playerguesslist):
+        j =0
+        while linear_search_recursive(code,playerguesslist[i]) and j <len(code):#for j in range(len(code1)):
+            if dct[i]:
+                if playerguesslist[i] == code[j]:
+                    pins['white'] +=1
+                    print(code,'code',playerguesslist[i],'playerguesslist[i]')
+
+                break
             j+=1
+        i+=1
 
     print(pins)
     guess(code)
+
+def linear_search_recursive(lst, target):
+    if lst == []:
+        return False
+    if lst[0] == target:
+        return True
+    else:
+        return linear_search_recursive(lst[1:],target)
 
 
 def simple_strategy():
@@ -53,7 +68,7 @@ def expected_size():
 
 def main():
     '''driver code'''
-    print(generate())
+    guess(['D', 'F', 'D', 'B'])
 
 
 
